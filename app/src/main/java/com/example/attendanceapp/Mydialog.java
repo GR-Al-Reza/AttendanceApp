cd80ee22bf5e98feacfcff4caa4129f4ce5c245e
GR-Al-Reza <alreza203267@gmail.com>
2022-03-01 01:22:55 +0600

    design part

--- app/src/main/java/com/example/attendanceapp/Mydialog.java
+++ app/src/main/java/com/example/attendanceapp/Mydialog.java
@@ -2,6 +2,7 @@ package com.example.attendanceapp;
 
 import android.app.Dialog;
 import android.content.Intent;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -36,6 +37,7 @@ public class Mydialog extends DialogFragment {
         { dialog=getAddClassDialog();}
         if(getTag().equals(STUDENT_ADD_DIALOG))
         {dialog=getAddStudentDialog();}
+        dialog.getWindow().setBackgroundDrawable(new ColorDrawable(android.graphics.Color.TRANSPARENT));
 
         return dialog;
     }
